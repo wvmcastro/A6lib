a504cee6e77ed62e4142a34a9e65e09bdeac1ae8
Wellington Castro <wellingtonvcastro@gmail.com>
2020-08-06 09:41:38 -0300

    adding min max functions

---
+++ A6Math.h
@@ -0,0 +1,11 @@
+#ifndef __A6_MATH_H
+#define __A6_MATH_H
+
+namespace math
+{
+    int max(int a, int b) { return (a > b) ? a : b;}
+    int min(int a, int b) { return (a > b) ? b : a;}
+}
+
+
+#endif
